4980e2b520192690675e75c8ed91e8202cd399bb
Mihai GHEOACE (94307) <mihai.gheoace@stud.acs.upb.ro>
2022-05-27 12:56:42 +0300

    functional auth server

---
+++ oauth-authorization-server/src/main/java/com/img/auth/OauthAuthorizationServerApplication.java
@@ -0,0 +1,37 @@
+package com.img.auth;
+
+import com.img.auth.config.KeycloakServerProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication(exclude = LiquibaseAutoConfiguration.class)
+@EnableConfigurationProperties({ KeycloakServerProperties.class })
+public class OauthAuthorizationServerApplication {
+
+    private static final Logger LOG = LoggerFactory.getLogger(OauthAuthorizationServerApplication.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthAuthorizationServerApplication.class, args);
+    }
+
+    @Bean
+    ApplicationListener<ApplicationReadyEvent> onApplicationReadyEventListener(ServerProperties serverProperties,
+                                                                               KeycloakServerProperties keycloakServerProperties) {
+
+        return (evt) -> {
+
+            Integer port = serverProperties.getPort();
+            String keycloakContextPath = keycloakServerProperties.getContextPath();
+
+            LOG.info("Embedded Keycloak started: http://localhost:{}{} to use keycloak", port, keycloakContextPath);
+        };
+    }
+}
---
+++ oauth-authorization-server/src/main/java/com/img/auth/config/RegularJsonConfigProviderFactory.java
@@ -0,0 +1,7 @@
+package com.img.auth.config;
+
+import org.keycloak.services.util.JsonConfigProviderFactory;
+
+public class RegularJsonConfigProviderFactory extends JsonConfigProviderFactory {
+
+}
