1b23c143aa2cc83484cddbb0cf0c7fc9653403de
Mihai GHEOACE (94307) <mihai.gheoace@stud.acs.upb.ro>
2023-01-25 19:20:15 +0200

    ensure large enough timeout for AsyncSupport

--- oauth-resource-server/src/main/java/com/img/resource/spring/WebConfig.java
+++ oauth-resource-server/src/main/java/com/img/resource/spring/WebConfig.java
@@ -1,10 +1,12 @@
 package com.img.resource.spring;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -12,6 +14,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = { "com.img.resource" })
+@Slf4j
 public class WebConfig implements WebMvcConfigurer {
     @Value("${base_fe_url}")
     String baseFEURL;
@@ -26,5 +29,12 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins(baseFEURL);
+    }
+
+    @Override
+    public void configureAsyncSupport(AsyncSupportConfigurer configurer) {
+//        this.configureAsyncSupport(configurer);
+        configurer.setDefaultTimeout(240000);
+        log.debug("set async web timeout");
     }
 }
