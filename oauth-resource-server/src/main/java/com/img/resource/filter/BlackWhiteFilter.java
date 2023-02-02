9abb410e9b0863cd6ba354b1cfc7eb95eae4234d
Mihai GHEOACE (94307) <mihai.gheoace@stud.acs.upb.ro>
2023-02-02 16:58:29 +0200

    understand time processing as log info

--- oauth-resource-server/src/main/java/com/img/resource/filter/BlackWhiteFilter.java
+++ oauth-resource-server/src/main/java/com/img/resource/filter/BlackWhiteFilter.java
@@ -3,14 +3,15 @@ package com.img.resource.filter;
 import com.img.resource.utils.Image;
 import com.img.resource.utils.Pixel;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.concurrent.Executor;
+import java.time.Duration;
+import java.time.Instant;
 
 @Slf4j
 public class BlackWhiteFilter extends AbstractFilter{
 
     public void applyFilterPh1(Image image, Image newImage, int start, int stop) {
+        Instant startTime = Instant.now();
         for (int i = start; i < stop; ++i) {
             for (int j = 0; j < image.width - 1; ++j) {
                 int gray = (int) (0.2126 * image.matrix[i][j].r +
@@ -20,6 +21,8 @@ public class BlackWhiteFilter extends AbstractFilter{
                 newImage.matrix[i][j] = new Pixel((char) gray, (char) gray, (char) gray, image.matrix[i][j].a);
             }
         }
+        Duration duration = Duration.between(startTime, Instant.now());
+        log.info("time processing " + (stop-start) + " lines of length:" + image.width + " duration:" + duration);
         log.debug("bw filter ph1:" + start);
     }
 }
