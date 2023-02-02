330347ad655a49cf6e3a66ff5e5d37e93a90b491
Mihai GHEOACE (94307) <mihai.gheoace@stud.acs.upb.ro>
2023-02-02 17:01:44 +0200

    clean Controller

--- oauth-resource-server/src/main/java/com/img/resource/web/controller/Controller.java
+++ oauth-resource-server/src/main/java/com/img/resource/web/controller/Controller.java
@@ -24,6 +24,7 @@ import java.util.Iterator;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 @Slf4j
 @RestController
@@ -40,18 +41,11 @@ public class Controller {
 
     @Async("executorOne")
     @PostMapping(value = "/filter", produces = MediaType.IMAGE_PNG_VALUE)
-    public CompletableFuture<ResponseEntity<byte[]>> filterImage(MultipartHttpServletRequest request, @AuthenticationPrincipal Jwt principal) throws ExecutionException, InterruptedException { //
-//        Instant start = Instant.now();
-//        r.acquire();
-        log.debug("debug message in image filter !!!!!!!!");
-//        return new WebAsyncTask<>(20 * 1000, () -> {
+    public CompletableFuture<ResponseEntity<byte[]>> filterImage(MultipartHttpServletRequest request, @AuthenticationPrincipal Jwt principal) { //
+        log.debug("debug message in image filter !");
 
         Iterator<String> itr = request.getFileNames();
-        log.debug("principal\n" + principal + "\nend-principal");
-        log.debug("principal pref username" + principal.getClaimAsString("preferred_username"));
-        principal.getClaims().forEach((key, value) -> {
-            System.out.println("claim key:" + key + " value:" + value);
-        });
+
         String userId = principal.getClaimAsString("sub");
 
         MultipartFile file;
@@ -81,33 +75,21 @@ public class Controller {
                 final Image input = imageFormatIO.bufferedToModelImage(bufferedImage);
                 final CompletableFuture<Image> res;
                 res = imgSrv.process(input, filterNames, filterParams);
-
                 BufferedImage image1;
 
-                image1 = imageFormatIO.modelToBufferedImage(res.get());
+                final Image image2 = res.get(24, TimeUnit.SECONDS);
                 log.debug("future of res obtained");
+                image1 = imageFormatIO.modelToBufferedImage(image2);
                 log.debug("buffered result image obtained");
 
                 final byte[] bytes;
                 bytes = imageFormatIO.bufferedToByteArray(image1);
                 log.debug("bytes result image obtained");
 
-//                final ImageEntity imageEntity = new ImageEntity();
-//                log.debug("save res image to db");
-//                imageEntity.setContent(bytes);
-//                imageEntity.setUserId(userId);
-//                log.debug("image usedId:" + imageEntity.getUserId());
-//                log.debug("image content size:" + imageEntity.getContent().length);
-//                imgRepo.save(imageEntity);
-//                if (imgRepo.findByUserId(userId).size() < 1) {
-//                    log.debug("cannot save image");
-//                    return internalServerError;
-//                }
 
                 log.debug("imaged has been saved");
-//            log.debug(Duration.between(start, Instant.now()).toMillis().toString());
                 return CompletableFuture.completedFuture(ResponseEntity.ok(bytes));
-            } catch (IOException | InterruptedException | ExecutionException e) {
+            } catch (IOException | InterruptedException | ExecutionException | TimeoutException e) {
                 e.printStackTrace();
                 log.debug(e.getMessage());
                 return internalServerError;
@@ -119,11 +101,6 @@ public class Controller {
                         .body(new byte[0])
         );
 
-        // https://medium.com/javarevisited/async-controllers-with-spring-boot-45bad3e66eea
-//        responseAsync.exceptionally(() -> {return ResponseEntity.badRequest().body(new byte[0]);});
-//todo seems that jmeter is closing connection too fast, recheck it actually does use the correct timeout
-        // check if the error happens after the value of 4 requests, which is the max pool of async executor
-//        return CompletableFuture.completedFuture(responseAsync.get());
     }
 
 }
